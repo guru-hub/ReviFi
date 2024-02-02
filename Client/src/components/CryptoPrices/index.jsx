caeb966f20cc791ecffaad505b701d1ef7c7c011
abhishekb740 <abhishekb740@gmail.com>
2024-02-02 10:45:01 +0530

    SymbolToId changed for Token Hlodings

--- Client/src/components/CryptoPrices/index.jsx
+++ Client/src/components/CryptoPrices/index.jsx
@@ -14,16 +14,16 @@ const CryptoPrices = () => {
 
 
   const SymbolToId = {
-    "BTC": "BTC",
-    "ETH": "ETH",
-    "BNB": "BNB",
-    "USDT": "USDT",
-    "SOL": "SOL",
-    "LTC": "LTC",
-    "XRP": "XRP",
-    "TRX": "TRX",
-    "ADA": "ADA",
-    "DOT": "DOT",
+    "BTC": "bitcoin",
+    "ETH": "ethereum",
+    "BNB": "binancecoin",
+    "USDT": "tether",
+    "SOL": "solana",
+    "LTC": "litecoin",
+    "XRP": "ripple",
+    "TRX": "tron",
+    "ADA": "cardano",
+    "DOT": "polkadot",
     ",": "%2C%20"
   }
 
