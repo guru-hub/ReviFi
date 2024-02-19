608dc4bc33381ab927444c9bd31370550af7730f
abhishekb740 <abhishekb740@gmail.com>
2024-02-20 02:07:00 +0530

    Home page styling changes

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -4,8 +4,8 @@ import Button from '@mui/material/Button';
 const Home = () => {
   return (
     <div style={{}} className={styles["container"]}>
-      <div className={styles["inner-container"]}>
-        <div className={styles["title"]}>
+      <div className="flex-col w-1/2 h-[400px]">
+        <div className="text-black">
           <div style={{ fontSize: '62px', fontWeight: '400' }} >
             Welcome to
           </div>
@@ -22,7 +22,7 @@ const Home = () => {
           </p>
           <p>Experience the power of AI-enhanced DeFi asset management.</p>
         </div>
-        <div className={`${styles["buttons"]} pt-10`}>
+        <div className="flex gap-[2rem] w-full pt-10">
           <Button style={{ color: 'white', background: 'linear-gradient(#0047aa, #0085b6)', width: '50%' }}>
             Join Telegram
           </Button>
